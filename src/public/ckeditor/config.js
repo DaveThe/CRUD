640efc6387a13c67b073187c9833efe9fc753213
david <d.tresoldi5@gmail.com>
2017-04-10 00:23:13 +0200

    -- add configurations to ckeditor

--- src/public/ckeditor/config.js
+++ src/public/ckeditor/config.js
@@ -3,9 +3,25 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
     config.language = 'it';
     // config.uiColor = '#AADC6E';
     config.removePlugins = 'flash,save,print,pagebreak,anchor,forms,about,preview,newpage,templates,find,div,bidi,language,colorbutton';
+    config.toolbarGroups = [
+        {name: 'document', groups: ['mode', 'document', 'doctools']},
+        {name: 'clipboard', groups: ['clipboard', 'undo']},
+        {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+        {name: 'forms', groups: ['forms']},
+        {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+        {name: 'links', groups: ['links']},
+        '/',
+        {name: 'insert', groups: ['insert']},
+        {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
+        {name: 'styles', groups: ['styles']},
+        {name: 'colors', groups: ['colors']},
+        {name: 'tools', groups: ['tools']},
+        {name: 'others', groups: ['others']},
+        {name: 'about', groups: ['about']}
+    ];
 };
